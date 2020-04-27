595e798ab597cd711d1390203ffcb15ad14f3e08
Anthony McLeod <mcleod333@gmail.com>
2020-04-26 20:22:40 -0400

    search for recommendation instead of ticker price

--- api.js
+++ api.js
@@ -16,15 +16,30 @@ router.route('/stock/:ticker').get(async (req, res) => {
     let url = `https://finance.yahoo.com/quote/${ticker}?p=${ticker}&.tsrc=fin-srch`;
 
     await page.goto(url);
-    // Wait for the page to finish rendering
-    await page.waitFor('#quote-market-notice', {timeout: 1000});
-    // Scrape the stock price from the page
-    let price = await page.evaluate(() => document.querySelector("#quote-header-info > div.Pos\\(r\\) > div > div > span").textContent);
-    await browser.close();
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
 
-    // Return the ticker and price
-    console.log(chalk.green(`Current stock price for ${ticker} is ${price}`));
-    res.send({ticker, price});
+    await autoScroll(page);
+    // Wait for the page to finish rendering
+    await page.waitFor('#Col2-8-QuoteModule-Proxy', {timeout: 5000});
+    await page.waitFor('#Col2-8-QuoteModule-Proxy > div > section > div > div > div', {timeout: 5000});
+
+    try {
+
+      let recommend = await page.evaluate(() => document.querySelector("#Col2-8-QuoteModule-Proxy > div > section > div > div > div").textContent);
+      await browser.close();
+
+      console.log(chalk.green(`Current recommendation for ${ticker} is ${recommend}`));
+      res.send({ticker, recommend});
+
+    } catch (e) {
+      await browser.close();
+      res.send(`Could not determine the recommendation for ${ticker}`);
+    }
+    
+    
 });
 
 // Handle the url parameter
@@ -32,5 +47,24 @@ router.param('ticker', (req, res, next, ticker) => {
     req.ticker = ticker.toUpperCase();
     next();
 });
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
 
 module.exports = router;
